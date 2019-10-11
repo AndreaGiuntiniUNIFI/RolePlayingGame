6532a8c69ff01f8eb2245c61c5d13842be76e494
AndreaGiuntiniUNIFI <andrea.giuntini@stud.unifi.it>
2019-10-11 18:29:19 +0200

    Adding test file to see if coveralls.io will work

---
+++ RolePlayingGame/src/test/java/com/rpg/character/controller/CharacterControllerTest.java
@@ -0,0 +1,14 @@
+package com.rpg.character.controller;
+
+import static org.junit.Assert.*;
+import static org.assertj.core.api.Assertions.*;
+import org.junit.Test;
+
+public class CharacterControllerTest {
+
+	@Test
+	public void test() {
+		assertThat("1").isEqualTo("1");
+	}
+
+}
