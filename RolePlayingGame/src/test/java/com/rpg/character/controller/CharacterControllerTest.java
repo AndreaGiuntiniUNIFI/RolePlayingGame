bc8394ddfc05e27656f365c9fcfad62c996c0c2a
AndreaGiuntiniUNIFI <andrea.giuntini@stud.unifi.it>
2019-10-12 15:15:28 +0200

    Adding Mockito dependency to gradle project

--- RolePlayingGame/src/test/java/com/rpg/character/controller/CharacterControllerTest.java
+++ RolePlayingGame/src/test/java/com/rpg/character/controller/CharacterControllerTest.java
@@ -1,11 +1,18 @@
 package com.rpg.character.controller;
 
-import static org.junit.Assert.*;
 import static org.assertj.core.api.Assertions.*;
 import org.junit.Test;
+import org.mockito.Mock;
+
+import com.rpg.character.view.CharacterView;
+
+import static org.mockito.Mockito.*;
 
 public class CharacterControllerTest {
-
+	
+	@Mock
+	CharacterView characterView;
+	
 	@Test
 	public void test() {
 		assertThat("1").isEqualTo("1");
