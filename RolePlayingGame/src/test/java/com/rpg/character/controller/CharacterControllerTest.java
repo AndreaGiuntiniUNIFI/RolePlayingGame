0dcb155bd8c499cf9da245d7b4080abf31db7364
AndreaGiuntiniUNIFI <andrea.giuntini@stud.unifi.it>
2019-10-12 15:44:20 +0200

    Downgrading to mockito version 2.23.4

--- RolePlayingGame/src/test/java/com/rpg/character/controller/CharacterControllerTest.java
+++ RolePlayingGame/src/test/java/com/rpg/character/controller/CharacterControllerTest.java
@@ -1,18 +1,30 @@
 package com.rpg.character.controller;
 
 import static org.assertj.core.api.Assertions.*;
+
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import com.rpg.character.view.CharacterView;
 
 import static org.mockito.Mockito.*;
 
 public class CharacterControllerTest {
-	
+
 	@Mock
 	private CharacterView characterView;
-	
+
+	@InjectMocks
+	private CharacterController characterController;
+
+	@Before
+	public void setup() {
+		MockitoAnnotations.initMocks(this);
+	}
+
 	@Test
 	public void test() {
 		assertThat("1").isEqualTo("1");
